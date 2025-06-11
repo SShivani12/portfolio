ce3bfa25899a0e345f3d4b3aa2bf5b6ea040ee59
SShivani12 <103356544+SShivani12@users.noreply.github.com>
2025-06-11 10:40:43 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PORTFOLIO from './PORTFOLIO.js'
+
+function App() {
+  return (
+    <div>
+      <PORTFOLIO /> {/* Render the Portfolio component */}
+    </div>
+  );
+}
+
+export default App;
